d4ca9209517bac93194fe43598a2e8a59d2a9e01
DiegoV8 <874280@unizar.es>
2025-03-01 14:19:07 +0100

    evitar conflictos merge

--- guignote/main.cpp
+++ guignote/main.cpp
@@ -1,11 +1,11 @@
 #include "mainwindow.h"
-#include "menu.h"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    menu w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
