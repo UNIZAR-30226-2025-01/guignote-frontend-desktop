36ad47145d3607e6ba8ac4fe3f9c2bb2371ed825
JoseManuVivas <870705@unizar.es>
2025-03-03 14:02:24 +0100

    Revert "Había un problema de sintaxis que había que arreglar"
    This reverts commit 751c64c16d4486364740b8c4774a8db6b2551c3c.

--- guignote/loginwindow.cpp
+++ guignote/loginwindow.cpp
@@ -192,7 +192,7 @@ LoginWindow::LoginWindow(QWidget *parent)
                         qDebug() << "Token recibido:" << token;
                         // Aquí se puede almacenar el token según se requiera.
                         // Luego se abre la ventana del menú principal.
-                        MenuWindow *menuWin = new MenuWindow();
+                        menu *menuWin = new menu();
                         menuWin->move(this->geometry().center() - menuWin->rect().center());
                         menuWin->show();
                         this->close();
